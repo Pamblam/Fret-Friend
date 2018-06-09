59b277daa7dcb5ce7431188ff61eb59980909b07
= <adelphia@protonmail.ch>
2018-06-08 20:32:52 -0400

    testing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,36 @@
 
-function readyPage(){
-	return new Promise(done=>{
-		var docReday = new Promise(d=>$(d));
-		var jsqlReday = new Promise(d=>jSQL.load(()=>{console.log('done'); done()}));
-		Promise.all([docReday,jsqlReday]).then(done);
-	});
-}
-
-readyPage().then(()=>{
-	initDB().then(setState);
+var DB = {};
+$(()=>{
+	$("#new_instrument_modal").modal({show: false, backdrop: 'static', keyboard: false});
+	initDB();
+	setState();
 });
 
 $(document).on('click', '.new_instrument', function(e){
 	e.preventDefault();
-	console.log('todo');
+	$("#new_instrument_modal").modal("show");
+	$("#addInstrumentButton").off("click").click(function(){
+		var err = msg=>{
+			$("#new_instrument_modal")[0].scrollTo(0,0);
+			$(".modalerroralert").find(".errortext").html(msg);
+			$(".modalerroralert").slideDown("fast", function(){
+				setTimeout(()=>$(".modalerroralert").slideUp("fast"),5000);
+			});
+		};
+		var name = $("#inst-name-input").val();
+		if(!name) return err("Enter a name for this instrument.");
+		if(DB.instruments[name]) return err("That name is taken please choose another one.");
+		var frets = parseInt($("#fret-cnt-input").val());
+		if(!frets) return err("Enter the number of frets the instrument has.");
+		if(frets>35) return err(frets+" frets is too many.");
+		var strings = parseInt($("#string-cnt-input").val());
+		if(!strings) return err("Enter the number of strings the instrument has.");
+		if(2>strings) return err(strings+" strings is not enough.");
+		if(strings>12) return err(strings+" strings is too many.");
+		var tuning = $("#tuning-input").val().split(",").map(a=>a.trim());
+		if(tuning.length<strings) return err("You did not enter enough notes for a "+strings+" stringed instrument.");
+		if(tuning.length>strings) return err("You entered too many notes for a "+strings+" stringed instrument.");
+	});
 });
 
 $(document).on('click', '.change_instrument', function(e){
@@ -30,131 +47,67 @@ $(document).on('click', '.open_page', function(e){
 
 function openPage(page){
 	$(".page").slideUp('fast', ()=>$("#"+page+"_page").slideDown());
-	jSQL.query("UPDATE `state` SET `tab` = ?").execute([page]);
-	jSQL.commit();
+	DB.state.tab=page;
+	saveDB();
 }
 
 function setInstrument(name){
-	getInstrument(name).then(inst=>{
-		if(!inst) return;
-		$(".current_instrument").html(name);
-		jSQL.query("UPDATE `state` SET `instrument` = ?").execute([name]); 
-		jSQL.commit();
-	});
+	if(!DB.instruments[name]) return;
+	$(".current_instrument").html(name);
+	DB.state.instrument = name;
+	saveDB();
 }
 
 function setState(){
-	var state = jSQL.query("SELECT * FROM `state`").execute().fetch("ASSOC");
 	$("#instruments_dropdown").empty();
-	var instruments = jSQL.query("SELECT `name` FROM `instruments`").execute().fetchAll("ASSOC");
-	instruments.forEach(inst=>$("#instruments_dropdown").append('<li><a href="#" class="change_instrument" data-instrument="'+encodeURIComponent(inst.name)+'">'+inst.name+'</li>'));
+	for(var name in DB.instruments) if(DB.instruments.hasOwnProperty(name)) $("#instruments_dropdown").append('<li><a href="#" class="change_instrument" data-instrument="'+encodeURIComponent(name)+'">'+name+'</li>')
 	$("#instruments_dropdown").append('<li><a href="#" class="new_instrument"><span class="glyphicon glyphicon-edit"></span> New Instrument</a></li>');
-	//setInstrument(state.instrument);
-	openPage(state.tab);
+	setInstrument(DB.state.instrument);
+	openPage(DB.state.tab);
 }
 
-function getInstrument(name){
-	return new Promise(done=>{
-		setTimeout(()=>{
-			var p = name ? new Promise(d=>d(name)) : new Promise(d=>setTimeout(()=>done(jSQL.query("SELECT `instrument` FROM `state`").execute().fetch("ASSOC").instrument),50));
-			p.then(n=>done(jSQL.query("SELECT * FROM `instruments` WHERE `name` = ?").execute([n]).fetch("ASSOC")));
-		},50);
-	});
+function initDB(){		
+	DB.state = localStorage.getItem('state');
+	if(!DB.state) DB.state = {instrument:'Guitar (Standard)', tab: 'home'}
+	else DB.state = JSON.parse(DB.state);
+	DB.instruments = localStorage.getItem('instruments');
+	if(!DB.instruments){
+		DB.instruments = {};
+		DB.instruments['Guitar (Standard)']={
+			frets: 24,
+			strings: 6,
+			tuning: ["E","B","G","D","A","E"],
+			dblFretMarkers: [12,24],
+			snglFretMarkers: [3,5,7,9,15,17,19,21]
+		};
+		DB.instruments['Bass (Standard)']={
+			frets: 24,
+			strings: 4,
+			tuning: ["G","D","A","E"],
+			dblFretMarkers: [12,24],
+			snglFretMarkers: [3,5,7,9,15,17,19,21]
+		};
+		DB.instruments['Ukulele (Standard)']={
+			frets: 18,
+			strings: 4,
+			tuning: ["A","E","C","G"],
+			dblFretMarkers: [12],
+			snglFretMarkers: [5,7,10,14]
+		};
+		DB.instruments['Mandolin (Standard)']={
+			frets: 20,
+			strings: 4,
+			tuning: ["E","A","D","G"],
+			dblFretMarkers: [12],
+			snglFretMarkers: [3,5,7,10,15]
+		};
+	}else DB.instruments = JSON.parse(DB.instruments);
+	saveDB();
 }
 
-function initDB(){
-	return new Promise(ddd=>{
-		jSQL.query("-- Generated with jSQL Devel on Friday, June 8th 2018, 2:37pm \n\
-		CREATE TABLE IF NOT EXISTS `state` (\n\
-			`instrument` VARCHAR(100) NOT NULL,\n\
-			`tab` ENUM(\"scales\", \"chords\", \"home\") NOT NULL DEFAULT \"scales\",\n\
-		)").execute();
-		if(!jSQL.query("SELECT * FROM `state`").execute().fetchAll("ASSOC").length){ 
-			console.log("Creating new state")
-			jSQL.query("INSERT INTO `state` VALUES (?, ?)").execute(['Guitar (Standard)', 'home']);
-		}
-		jSQL.query("-- Generated with jSQL Devel on Friday, June 8th 2018, 1:14pm \n\
-		CREATE TABLE IF NOT EXISTS `instruments` (\n\
-			`name` VARCHAR(100) NOT NULL,\n\
-			`frets` NUMERIC NOT NULL,\n\
-			`strings` NUMERIC NOT NULL,\n\
-			`tuning` ARRAY NOT NULL,\n\
-			`doubleFretMarkers` ARRAY NOT NULL,\n\
-			`singleFretMarkers` ARRAY NOT NULL\n\
-		)").execute();
-		jSQL.commit();
-
-		var promises = [];
-
-		promises.push(new Promise(done=>{
-			getInstrument('Guitar (Standard)').then(inst=>{
-				if(inst) return done();
-				jSQL.query("INSERT INTO `instruments` VALUES (?, ?, ?, ? ,?, ?)").execute([
-					'Guitar (Standard)', // name
-					24, // frets
-					6, // strings
-					["E","B","G","D","A","E"], // tuning
-					[12,24], // double fret markers
-					[3,5,7,9,15,17,19,21] // single fret markers
-				]);
-				jSQL.commit();
-				done();
-			});
-		}));
-
-		promises.push(new Promise(done=>{
-			getInstrument('Bass (Standard)').then(inst=>{
-				if(inst) return done();
-				jSQL.query("INSERT INTO `instruments` VALUES (?, ?, ?, ?, ?, ?)").execute([
-					'Bass (Standard)', // name
-					24, // frets
-					4, // strings
-					["G","D","A","E"], // tuning
-					[12,24], // double fret markers
-					[3,5,7,9,15,17,19,21] // single fret markers
-				]);
-				jSQL.commit();
-				done();
-			});
-		}));
-		
-		promises.push(new Promise(done=>{
-			getInstrument('Ukulele (Standard)').then(inst=>{
-				if(inst) return done();
-				jSQL.query("INSERT INTO `instruments` VALUES (?, ?, ?, ?, ?, ?)").execute([
-					'Ukulele (Standard)', // name
-					18, // frets
-					4, // strings
-					["A","E","C","G"], // tuning
-					[12], // double fret markers
-					[5,7,10,14] // single fret markers
-				]);
-				jSQL.commit();
-				done();
-			});
-		}));
-		
-		promises.push(new Promise(done=>{
-			getInstrument('Mandolin (Standard)').then(inst=>{
-				if(inst) return done();
-				jSQL.query("INSERT INTO `instruments` VALUES (?, ?, ?, ?, ?, ?)").execute([
-					'Mandolin (Standard)', // name
-					20, // frets
-					4, // strings
-					["E","A","D","G"], // tuning
-					[12], // double fret markers
-					[3,5,7,10,15] // single fret markers
-				]);
-				jSQL.commit();
-				done();
-			});
-		}));
-
-		Promise.all(promises).then(()=>{
-			jSQL.commit();
-			ddd();
-		});
-		
-	});
-	
+function saveDB(){
+	for(var table in DB){
+		if(!DB.hasOwnProperty(table)) continue;
+		localStorage.setItem(table, JSON.stringify(DB[table]));
+	}
 }
